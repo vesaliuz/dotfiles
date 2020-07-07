d97d91ba67ca50a9e860b9ba3ed39b1bea7a8bef
paperbenni <paperbenni@gmail.com>
2020-07-07 03:51:36 -0400

    new ncmpcpp config files

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -15,8 +15,12 @@ countdown() {
 for i in *".mkv"
 do
     countdown 20 "Press Ctrl-C to stop"
-    echo "Continue"
-    ffmpeg -i "$i" -map 0:0 -map 0:2 -map 0:3 -c:v libx265 -crf 28 -preset fast -c:a copy -c:s copy  "${i%%".mkv"}_x265_AAC.mkv"
+    echo "####################"
+    printf "/n"
+    echo "Converting $i"
+    echo "####################"
+    printf "\n\n"
+    ffmpeg -i "$i"  -map 0:0 -map 0:1 -map 0:3 -c:v libx265 -crf 25 -preset medium -c:a copy -c:s copy  "${i%%".mkv"}_x265_AAC.mkv"
     mv "$i" "${i%%".mkv"}.old"
-    
+    echo "####################\n\nDONE\n"
 done
