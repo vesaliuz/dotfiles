1f691f1023b1016be5ace1bc7b06660bdecd8a42
paperbenni <paperbenni@gmail.com>
2020-07-21 19:30:09 -0400

    aaded new polybar config files

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -20,7 +20,7 @@ do
     echo "Converting $i"
     echo "####################"
     printf "\n\n"
-    ffmpeg -i "$i"  -map 0 -c:v libx265 -crf 25 -preset fast -c:a libfdk_aac -b:a 160k -c:s copy  "${i%%".mkv"}_x265_AAC.mkv"
+    ffmpeg -i "$i"  -map 0 -c:v libx265 -crf 25 -preset fast -c:a libfdk_aac -b:a 96k -c:s copy  "${i%%".mkv"}_x265_AAC.mkv"
     
     # To set sub as default
 #  ffmpeg -i "$i"  -map 0:0 -map 0:1 -map 0:2 -c:v libx265 -crf 25 -preset fast -c:a libfdk_aac -b:a 192k -c:s copy -disposition:s:0 default "${i%%".mkv"}_x265_AAC.mkv"
--- scripts/merge.sh
+++ scripts/merge.sh
@@ -2,7 +2,7 @@
 
 for i in *".mkv"
 do
-    mkvmerge -o "${i%%".mkv"}_x265_AAC.mkv" -d 0 -a jpn -s 3 --default-track 3:1\
+    mkvmerge -o "${i%%".mkv"}.remerged.mkv" -d 0 -a 1 -s 3 --default-track 3:1\
     "$i"
     mv "$i" "${i%%".mkv"}.old"
 done
