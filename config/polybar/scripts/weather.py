98ddbbdc9112e076fa80cbf4de75d724fc3f8a70
paperbenni <paperbenni@gmail.com>
2020-07-22 11:07:09 -0400

    added polybar herbsluft config files

--- config/polybar/scripts/weather.py
+++ config/polybar/scripts/weather.py
@@ -15,23 +15,34 @@
 
 import requests
 
-CITY = "2803138"
+CITY = "3893894"
 API_KEY = "756edce7e9d4c385ef9499a53492678c"
 UNITS = "Metric"
 UNIT_KEY = "C"
 #UNIT_KEY = "F"
+#LANG = "pt"
 LANG = "en"
 #LANG = "nl"
 #LANG = "hu"
 
 REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+
 try:
     # HTTP CODE = OK
     if REQ.status_code == 200:
         CURRENT = REQ.json()["weather"][0]["description"].capitalize()
         TEMP = int(float(REQ.json()["main"]["temp"]))
-        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+
+# This is the default setup.
+#        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY)) 
+
+
+# The line below, just show or "print" the weather fetch result in °C without any string. 
+# So that, I get more room on the polybar (It's perfect if you own a Laptop).
+        print("{} °{}".format(TEMP, UNIT_KEY)) 
     else:
-        print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
+#        print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
+        print("Error to fetch your CITY CODE, in weather.py") 
+        
 except (ValueError, IOError):
     print("Error: Unable print the data")
--- scripts/merge.sh
+++ scripts/merge.sh
@@ -2,7 +2,7 @@
 
 for i in *".mkv"
 do
-    mkvmerge -o "${i%%".mkv"}.remerged.mkv" -d 0 -a 1 -s 3 --default-track 3:1\
-    "$i"
+    mkvmerge -o "${i%%".mkv"}.remerged.mkv" -d 0 -a jpn -s eng \
+    "$i" 
     mv "$i" "${i%%".mkv"}.old"
 done
