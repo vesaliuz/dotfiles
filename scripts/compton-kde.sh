362a0e4306999c564eda45fa2caf146d806b8a97
Andreas Vesaliuz <vesaliuz@protonmail.com>
2020-09-08 21:51:14 -0300

    updated dotfiles

--- scripts/compton-kde.sh
+++ scripts/compton-kde.sh
@@ -5,15 +5,15 @@
 #### V.0.1 2020-01-03
 
 ## Terminates any running compton session
-killall -q compton
+killall -q picom
 
 ## Wait until the processes have been shut down
-while pgrep -u $UID -x compton > /dev/null; do sleep 1; done
+while pgrep -u $UID -x picom > /dev/null; do sleep 1; done
 
 desktop=$(echo $DESKTOP_SESSION)
 
-if [ "$desktop" == '/usr/share/xsessions/openbox-kde' ] ; then
-    /usr/bin/compton -b --config $HOME/.config/compton/compton.conf
+if [ "$desktop" == '/usr/share/xsessions/plasma' ] ; then
+    /usr/bin/picom -b --experimental-backends --config $HOME/.config/picom.conf
 fi
 
 ###EOF
--- scripts/convert.sh
+++ scripts/convert.sh
@@ -20,10 +20,10 @@ do
     echo "Converting $i"
     echo "####################"
     printf "\n\n"
-#    ffmpeg -i "$i"  -map 0 -c:v libx265 -crf 25 -preset fast -c:a libfdk_aac -b:a 96k -c:s copy  "${i%%".mkv"}_x265_AAC.mkv"
+ffmpeg -i "$i"  -map 0 -c:v libx265 -crf 25 -preset fast -c:a libopus -b:a 128k -c:s copy  "${i%%".mkv"}_x265_Opus.mkv"
     
     # To set sub as default
- ffmpeg -i "$i"  -map 0 -c:v libx265 -crf 25 -preset fast -c:a copy -c:s copy -disposition:s:0 default "${i%%".mkv"}_x265_AAC.mkv"
+# ffmpeg -i "$i"  -map 0 -c:v libx265 -crf 25 -preset fast -c:a libopus -b:a 128k -c:s copy -disposition:s:0 default "${i%%".mkv"}_x265_AAC.mkv"
     
     # Downmix to stereo
     # ffmpeg -i "$i"  -map 0 -c:v libx265 -crf 23 -preset medium -c:a libfdk_aac -ac 2 -c:s copy  "${i%%".mkv"}_x265_AAC.mkv"
